483fdcc87717f901cb344f72e52bc9ed6aaec97d
stay0nly <derkach.dmt@gmail.com>
2022-06-01 13:47:41 +0800

    Map function and notes

---
+++ src/notes.js
@@ -0,0 +1,39 @@
+const notes = [
+    {
+      key: 1,
+      title: "Chris Pine",
+      content:
+        "Programming isn't about what you know; it's about what you can figure out."
+    },
+    {
+      key: 2,
+      title: "Dennis Ritchie",
+      content:
+        "The only way to learn a new programming language is by writing programs in it."
+    },
+    {
+      key: 3,
+      title: "Joyce Wheeler",
+      content:
+        "Sometimes it's better to leave something alone, to pause, and that's very true of programming."
+    },
+    {
+      key: 4,
+      title: "Andrew Hunt",
+      content:
+        "In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them."
+    },
+    {
+      key: 5,
+      title: "Burt Rutan",
+      content: "Testing leads to failure, and failure leads to understanding."
+    },
+    {
+      key: 6,
+      title: "Thomas Fuchs",
+      content: "The best error message is the one that never shows up."
+    }
+  ];
+  
+  export default notes;
+  
